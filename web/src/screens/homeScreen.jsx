eae085a9105b4a31b3df5fc7506e538e681f4d04
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-26 23:04:32 +0000

    creating statefor item , a function to get item using axios  an d displaying the content

--- web/src/screens/homeScreen.jsx
+++ web/src/screens/homeScreen.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FormComponent from './form'
+import axios from 'axios'
 
 export default function HomeScreen() {
+  const [item, setItem] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    axios.get('http://localhost:5000/')
+      .then((res) => {
+        console.log(res.data.list)
+        setItem(res.data.list)
+      })
+  }
+
   return (
     <div style={styles.mainContainer}>
       <h1>hello</h1>
       <div style={styles.container}>
-       <FormComponent/>
+        <section style={styles.formContainer}>
+          <FormComponent />
+        </section>
+        <br /><br />
+        <section style={styles.formContainer}>
+          {item.map((item)=>(
+          <div key={item._id}>
+            <h1>{item.name}</h1>
+          </div>
+          
+          ))}
+        </section>
       </div>
     </div>
   )
@@ -15,8 +41,12 @@ export default function HomeScreen() {
 const styles = {
   mainContainer: {
     backgroundColor: 'dodgerblue',
-    height: '100vh',
+    minHeight: '100vh',
     padding: 10,
   },
 
+ 
+  formContainer: {
+    width: '100%'
+  }
 }
