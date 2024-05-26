17c9b0baa7a0460e77bd7a2bee0c72b452702504
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-26 22:07:24 +0000

    adding input and styling to form

--- web/src/screens/form.jsx
+++ web/src/screens/form.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import InputComponent from '../components/inputComponent'
 
 export default function FormComponent() {
-  return (
-    <div>
-   
-    </div>
-  )
+    return (
+        <div style={styles.container}>
+            <div style={styles.inputContainer}>
+                <InputComponent className={'largeInput'} placeholder={'Enter title here'} label={'Title'} />
+            </div>
+        </div>
+    )
+}
+
+
+const styles = {
+inputContainer:{
+    
+}
 }
