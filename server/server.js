e1a36315d2587a36df28bb12b77b3861ac656e86
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-26 18:55:20 +0000

    mongoose connection to database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 5000
+
+
+const db_name = 'mongodb://localhost/todolistNode'
+const db = mongoose.connection
+
+mongoose.connect(db_name)
+
+db.on('error', (err) => console.error('Error Connecting to database', err))
+db.once('open', () => console.log('Successfully Connected to database'))
+
+
+
+
+
+
+
 
 app.listen(port, () => console.log(`Server is listeninhg from http://localhost:${port}`))
