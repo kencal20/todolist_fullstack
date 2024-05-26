7cfbeedaad7927ce203f0a88c27cd239c80d7c25
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-26 21:08:15 +0000

    form component init and rendering in the homeScreen

--- web/src/screens/homeScreen.jsx
+++ web/src/screens/homeScreen.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import FormComponent from './form'
 
 export default function HomeScreen() {
   return (
     <div style={styles.mainContainer}>
-      
+      <h1>hello</h1>
+      <div style={styles.container}>
+       <FormComponent/>
+      </div>
     </div>
   )
 }
 
-const styles={
-  mainContainer:{
+const styles = {
+  mainContainer: {
+    backgroundColor: 'dodgerblue',
+    height: '100vh',
+    padding: 10,
+  },
 
-  }
 }
