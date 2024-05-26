9905bf9eaf36d3dd789e0ec080c5c8c80fda560f
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-26 22:18:37 +0000

    adding all input fields into the page

--- web/src/screens/form.jsx
+++ web/src/screens/form.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import InputComponent from '../components/inputComponent'
 
+
 export default function FormComponent() {
     return (
         <div style={styles.container}>
             <div style={styles.inputContainer}>
-                <InputComponent className={'largeInput'} placeholder={'Enter title here'} label={'Title'} />
+                <InputComponent
+                    className={'largeInput'}
+                    placeholder={'Enter title here'}
+                    label={'Title'}
+                    required={true}
+                />
+            </div>
+            <div style={styles.inputContainer}>
+                <InputComponent
+                    className={'largeInput'}
+                    placeholder={'Enter Description here'}
+                    label={'Description'}
+                />
+            </div>
+            <div style={styles.inputContainer}>
+                <InputComponent
+                    className={'smallInput'}
+                    type={'date'}
+                    label={'Schedule Date'}
+                    required={true}
+                />
+            </div>
+            <div style={styles.inputContainer}>
+                <InputComponent
+                    className={'smallInput'}
+                    type={'time'}
+                    label={'Schedule Title'}
+                    required={true}
+                />
             </div>
         </div>
     )
@@ -13,7 +42,16 @@ export default function FormComponent() {
 
 
 const styles = {
-inputContainer:{
-    
-}
+    container: {
+        backgroundColor: 'grey',
+        width: '100%',
+        borderRadius: 20
+    },
+    inputContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        fontWeight: '900',
+        padding: 20,
+        fontSize: 18
+    }
 }
