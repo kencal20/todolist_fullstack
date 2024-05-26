349935cd311978f71fd336f899587b5f8d9c0110
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-26 18:57:02 +0000

    adding  middlewares

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 const port = 5000
+
+app.use(express.json())
+app.use(cors())
 
 
 const db_name = 'mongodb://localhost/todolistNode'
