34d8df1a78ab17b7ecbd91e3477282225a7bb0b0
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-05-27 08:56:02 +0000

    adding styling to item Component

--- web/src/components/item.jsx
+++ web/src/components/item.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-export default function Item({item}) {
+export default function Item({ item }) {
     return (
-        <div >
-            {item.name}
+        <div style={styles.container}>
+            <div style={styles.itemName}>
+               <b> {item.name}</b>
+            </div>
         </div>
     )
 }
 
-
+const styles = {
+    container: {
+        padding: 20,
+        backgroundColor: 'green',
+        borderRadius: 20,
+        
+    },
+    itemName:{
+        fontSize:20,
+        color:'white',
+  
+    }
+}
